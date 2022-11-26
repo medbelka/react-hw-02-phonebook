08e02387935236acca311883594cb1d1fda5c3ce
medbelka <margopv@gmail.com>
2022-11-27 00:58:22 +0200

    smth changed in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ import styles from '../components/App.module.css';
 class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: '',
     }
@@ -22,15 +22,24 @@ class App extends Component {
     }))
   }
 
+  // createButtonAlert = (name) =>
+  //   Alert.alert(
+  //     "Warning",
+  //     `${name} is already in contacts`,
+  //     [
+  //       { text: "OK", onPress: () => console.log("OK Pressed") }
+  //     ]
+  //   );
+
   addContact = (name, number) => {
-    const newContact = {
-      id: nanoid(),
-      name,
-      number,
-    }
-    this.setState(prevState => ({
-      contacts: [newContact, ...prevState.contacts]
-    }))
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      }
+      this.setState(prevState => ({
+        contacts: [newContact, ...prevState.contacts]
+      }))
   }
 
   changeFilter = e => {
